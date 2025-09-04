4d6fb3486f94acfb916e2e25ee0e4147698acbb3
Ana3Lu <anaquiva@unisabana.edu.co>
2025-09-04 16:51:49 -0500

    Reemplazo proyecto anterior por nuevo

--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Redirect } from "expo-router";
 
-const styles = StyleSheet.create({
-  container: { 
-    flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center',
-    backgroundColor: "#f0f0f0"
-    },
-    text: {
-      fontSize: 30
-    }
-})
-
-//image
-//text input 
-//button-> TouchableOpacity
-
-export default function HomeScreen() {
-  return <View
-  style={styles.container}>
-
-    <Text>
-      Hola mundo
-    </Text>
-  </View>
+export default function Index() {
+  return <Redirect href={"/(auth)/login"}/>
 }
